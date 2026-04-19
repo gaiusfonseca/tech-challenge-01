3e718e24d6952e5d1ed074806a06698816ed7641
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-04-19 19:24:09 -0300

    chore: adicionando a coluna user_type na tabela do banco de dados e alterando do script de insert para conter mais registros.

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,2 +1,8 @@
-INSERT INTO `USER` (email, `name`, `login`, `password`, `address`, last_modified) 
-VALUES ('pedrinho@gmail.com', 'pedro', 'pcaldeira', 'pcdr1234', 'rua das acácias', '2026-04-07');
+INSERT INTO `USER` (`login`, `password`, email, `name`, `address`, last_modified, user_type) 
+VALUES 
+    ('pedro321', 'X321kHEz', 'pedro@gmail.com', 'pedro', 'rua das acácias, 12', '2026-04-01', 'customer'),
+    ('lucasXyz', 'Wa3wuQy4', 'lucas@gmail.com', 'lucas', 'rua das oliveiras, 22', '2026-04-02', 'customer'),
+    ('maria1010', 'M4YP14uF', 'maria@gmail.com', 'maria', 'rua das cerejeiras, 32', '2026-04-03', 'customer'),
+    ('joaoBigBoss', 'udWP50kX', 'joao.bosco@gmail.com', 'joao', 'rua dos ipes, 42', '2026-04-04', 'owner'),
+    ('marianaDona', '3A49yr9C', 'mariana.nascimento@gmail.com', 'mariana', 'rua das magnólias, 52', '2026-04-05', 'owner'),
+    ('maria2020', 'bxBc99VY', 'maria.joana@gmail.com', 'maria', 'rua dos mandacarus, 62', '2026-04-03', 'owner');
--- src/test/resources/schema-test.sql
+++ src/test/resources/schema-test.sql
@@ -2,9 +2,10 @@
 CREATE TABLE IF NOT EXISTS `USER`(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(40) UNIQUE NOT NULL,
-    `name` VARCHAR(40) NOT NULL,
     `login` VARCHAR(40) NOT NULL,
     `password` VARCHAR(40) NOT NULL,
+    `name` VARCHAR(40) NOT NULL,
     `address` VARCHAR(255),
-    last_modified DATE
+    last_modified DATE NOT NULL,
+    user_type VARCHAR(40) NOT NULL
 );
