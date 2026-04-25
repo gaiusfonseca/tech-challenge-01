789bc58aa89dfef030df46fd9660264049fa544c
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-04-25 17:24:10 -0300

    refactor: alterando a estrutura da tabela User e respectivos dados para testes.

--- src/test/resources/schema-test.sql
+++ src/test/resources/schema-test.sql
@@ -2,10 +2,10 @@
 CREATE TABLE IF NOT EXISTS `USER`(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(40) UNIQUE NOT NULL,
-    `login` VARCHAR(40) NOT NULL,
-    `password` VARCHAR(40) NOT NULL,
+    `login` VARCHAR(40) UNIQUE NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
     `name` VARCHAR(40) NOT NULL,
     `address` VARCHAR(255),
     last_modified DATE NOT NULL,
-    user_type VARCHAR(40) NOT NULL
+    role VARCHAR(40) NOT NULL
 );
