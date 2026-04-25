7cc890a42213fb536b2598435a766f9117fcb42c
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-04-25 17:22:10 -0300

    feat: implementando AuthenticationService.

--- src/main/java/br/gaius/restaurant/services/AuthenticationService.java
+++ src/main/java/br/gaius/restaurant/services/AuthenticationService.java
@@ -1,9 +1,34 @@
 package br.gaius.restaurant.services;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+
+import br.gaius.restaurant.entities.User;
+import br.gaius.restaurant.exceptions.InvalidPasswordException;
+import br.gaius.restaurant.exceptions.UserNotFoundException;
+import br.gaius.restaurant.repositories.UserRepository;
 
 @Service
 public class AuthenticationService {
 
+    private UserRepository repository;
+
+    public AuthenticationService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public void authenticate(User candidate){
+        User user = repository.findByLogin(candidate.getLogin())
+            .orElseThrow(() -> new UserNotFoundException(candidate.getLogin()));
+
+        if(BCrypt.checkpw(candidate.getPassword(), user.getPassword()) == false){
+            throw new InvalidPasswordException(candidate.getLogin(), candidate.getPassword());
+        }
+
+        return;
+    }
 
 }
