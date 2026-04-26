c8a4b3bb4807fd7d89b8cfcef4ca575f8aa7f2e0
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-04-25 23:11:47 -0300

    feat: extraindo métodos de validação e acrescentando validação para login único.

--- src/main/java/br/gaius/restaurant/services/UserService.java
+++ src/main/java/br/gaius/restaurant/services/UserService.java
@@ -1,51 +1,65 @@
 package br.gaius.restaurant.services;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 import br.gaius.restaurant.dtos.ChangePasswordDTO;
 import br.gaius.restaurant.dtos.CreateUserDTO;
 import br.gaius.restaurant.dtos.UpdateUserDTO;
 import br.gaius.restaurant.entities.User;
-import br.gaius.restaurant.entities.UserFactory;
+import br.gaius.restaurant.entities.UserMapper;
 import br.gaius.restaurant.exceptions.DuplicatedEmailException;
-import br.gaius.restaurant.exceptions.InvalidPasswordException;
+import br.gaius.restaurant.exceptions.DuplicatedLoginException;
 import br.gaius.restaurant.repositories.UserRepository;
 
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
+    private final UserRepository repository;
+    private final UserMapper mapper;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserService(UserRepository repository, UserMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
     }
 
     public Optional<User> findById(Long id) {
-        return userRepository.findById(id);
+        return repository.findById(id);
     }
 
-    public List<User> findByName(String name, int page, int size){
-        if(page < 1) page = 1;
-        if(size < 1) size = 10;
+    public List<User> findByName(String name, int page, int size) {
+        if (page < 1){
+            page = 1;
+        }
+
+        if (size < 1){
+            size = 10;
+        }
+
         int offset = getOffset(page, size);
-        return userRepository.findByName(name, size, offset);
+        return repository.findByName(name, size, offset);
     }
 
-    public List<User> findByName(String name){
+    public List<User> findByName(String name) {
         int page = 1;
         int size = 10;
         return findByName(name, page, size);
     }
 
     public List<User> findAll(int page, int size) {
-        if(page < 1) page = 1;
-        if(size < 1) size = 10;
+        if (page < 1){
+            page = 1;
+        }
+
+        if (size < 1){
+            size = 10;
+        }
+
         int offset = getOffset(page, size);
-        return userRepository.findAll(size, offset);
+        return repository.findAll(size, offset);
     }
 
     public List<User> findAll() {
@@ -54,39 +68,63 @@ public class UserService {
         return findAll(page, size);
     }
 
-    public User save(CreateUserDTO requestBody){
-        UserFactory factory = UserFactory.of(requestBody.userType());
-        User userEntity = factory.fromCreateRequest(requestBody);
-        return userRepository.save(userEntity).orElseThrow();
-    }
+    public User save(CreateUserDTO dto) {
+        User user = mapper.from(dto);
 
-    public User update(UpdateUserDTO requestBody){
-        UserFactory factory = UserFactory.of(requestBody.userType());
-        User userEntity = factory.fromUpdateRequest(requestBody);
-        if(userRepository.count(requestBody.email()) > 0) throw new DuplicatedEmailException(userEntity.getEmail());
-        return userRepository.update(userEntity).orElseThrow();
-    }
-
-    public void delete(Long id){
-        userRepository.delete(id);
-    }
-
-    public Long count(String email){
-        return userRepository.count(email);
-    }
-
-    public void changePassword(Long id, ChangePasswordDTO requestBody) {
-        User user = userRepository.findById(id).orElseThrow();
-        
-        if(user.passwordMatches(requestBody.oldPassword())){
-            userRepository.updatePassword(id, requestBody.newPassword(), LocalDate.now());
-            return;
+        if(isDuplicatedEmail(dto.email())){
+            throw new DuplicatedEmailException(dto.email());
         }
 
-        throw new InvalidPasswordException(id, requestBody.oldPassword());
+        if(isDuplicatedLogin(dto.login())){
+            throw new DuplicatedLoginException(dto.login());
+        }
+
+        return repository.save(user).orElseThrow();
     }
 
-    private static int getOffset(int page, int size){
+    public User update(UpdateUserDTO dto) {
+        User user = mapper.from(dto);
+
+        if(isDuplicatedEmail(dto.email())){
+            throw new DuplicatedEmailException(dto.email());
+        }
+
+        if(isDuplicatedLogin(dto.login())){
+            throw new DuplicatedLoginException(dto.login());
+        }
+
+        return repository.update(user).orElseThrow();
+    }
+
+    public void delete(Long id) {
+        repository.delete(id);
+    }
+
+    public void changePassword(ChangePasswordDTO dto) {
+        User user = repository.findByLogin(dto.login()).get();
+
+        String hashedPassword = BCrypt.hashpw(dto.newPassword(), BCrypt.gensalt());
+
+        repository.updatePassword(user.getId(), hashedPassword);
+    }
+
+    private int getOffset(int page, int size) {
         return (page - 1) * size;
+    }
+
+    private boolean isDuplicatedEmail(String email){
+        if (repository.count("email", email) > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isDuplicatedLogin(String login){
+        if (repository.count("login", login) > 0) {
+            return true;
+        }
+
+        return false;
     }
 }
