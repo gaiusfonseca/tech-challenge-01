d1fc528a024ab28b5db4360c539c4d09d2ec0a41
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-04-25 23:07:57 -0300

    test: atualizando testes para considerar a nova versão de repository.count(fieldName, value).

--- src/test/java/br/gaius/restaurant/services/UserServiceTest.java
+++ src/test/java/br/gaius/restaurant/services/UserServiceTest.java
@@ -1,27 +1,54 @@
 package br.gaius.restaurant.services;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.test.context.ActiveProfiles;
 
+import br.gaius.restaurant.dtos.CreateUserDTO;
+import br.gaius.restaurant.dtos.UpdateUserDTO;
+import br.gaius.restaurant.entities.Role;
+import br.gaius.restaurant.entities.User;
+import br.gaius.restaurant.entities.UserMapper;
+import br.gaius.restaurant.exceptions.DuplicatedEmailException;
+import br.gaius.restaurant.exceptions.DuplicatedLoginException;
 import br.gaius.restaurant.repositories.UserRepository;
 
 @ActiveProfiles("test")
 public class UserServiceTest {
 
     private UserRepository repository;
+    private UserMapper mapper;
     private UserService service;
 
     @BeforeEach
     void setup() {
         repository = mock(UserRepository.class);
-        service = new UserService(repository);
+        mapper = new UserMapper();
+        service = new UserService(repository, mapper);
+    }
+
+    @Test
+    void shouldFindById() {
+        // given
+        Long id = 1L;
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        // when
+        service.findById(id);
+
+        // then
+        verify(repository).findById(anyLong());
     }
 
     @Test
@@ -114,67 +141,124 @@ public class UserServiceTest {
     }
 
     @Test
-    void shouldSaveWhenNoDuplicateEmail() {
+    void shouldSaveWhenNoDuplicateEmailAndLogin() {
         // given
+        String notDuplicatedEmail = "jacinto@test.com.br";
+        CreateUserDTO dto = new CreateUserDTO("jacintoXT", "nqEag0T2", notDuplicatedEmail, "jacinto",
+                "rua dos pés de jaca, 146", Role.OWNER);
+
+        User user = mapper.from(dto);
+        User userWithId = User.builder(user).withId(7L).build();
+
+        when(repository.save(user)).thenReturn(Optional.of(userWithId));
 
         // when
+        service.save(dto);
 
         // then
-        fail("Not yet implemented");
+        verify(repository).save(user);
     }
 
     @Test
     void shouldThrowDuplicatedEmailExceptionWhenSaveDuplicatedEmail() {
         // given
+        String fieldName = "email";
+        String duplicatedEmail = "pedro@gmail.com";
+        CreateUserDTO dto = new CreateUserDTO("jacintoXT", "nqEag0T2", duplicatedEmail, "jacinto",
+                "rua dos pés de jaca, 146", Role.OWNER);
+        when(repository.count(fieldName, duplicatedEmail)).thenReturn(1L);
 
         // when
+        Executable saveShouldThrowException = () -> service.save(dto);
 
         // then
-        fail("Not yet implemented");
+        assertThrows(DuplicatedEmailException.class, saveShouldThrowException);
+        verify(repository).count(fieldName, duplicatedEmail);
     }
 
     @Test
-    void shouldUpdateUserWhenEmailIsNotDuplicated() {
+    void shouldThrowDuplicatedLoginExceptionWhenSaveDuplicatedLogin() {
         // given
+        String fieldName = "login";
+        String duplicatedLogin = "pedro321";
+        CreateUserDTO dto = new CreateUserDTO(duplicatedLogin, "nqEag0T2", "jacinto@test.com.br", "jacinto",
+                "rua dos pés de jaca, 146", Role.OWNER);
+        when(repository.count(fieldName, duplicatedLogin)).thenReturn(1L);
 
         // when
+        Executable saveShouldThrowException = () -> service.save(dto);
 
         // then
-        fail("Not yet implemented");
+        assertThrows(DuplicatedLoginException.class, saveShouldThrowException);
+        verify(repository).count(fieldName, duplicatedLogin);
     }
 
     @Test
-    void shouldThrowDuplicatedEmailExceptionWhenUpdateEmailDuplicated() {
+    void shouldUpdateWhenNoDuplicateEmailAndLogin() {
         // given
+        Long id = 7L;
+
+        UpdateUserDTO dto = new UpdateUserDTO(id, "jacintoXT", "jacinto@test.com.br", "jacinto",
+                "rua dos pés de jaca, 146", Role.OWNER);
+
+        User user = mapper.from(dto);
+
+        when(repository.update(user)).thenReturn(Optional.of(user));
 
         // when
+        service.update(dto);
 
         // then
-        fail("Not yet implemented");
+        verify(repository).update(user);
     }
 
     @Test
-    void shouldDeleteWhenDelete(){
+    void shouldThrowDuplicatedEmailExceptionWhenDuplicatedEmail() {
+        // given
+        String fieldName = "email";
+        String duplicatedEmail = "pedro@gmail.com";
+
+        UpdateUserDTO dto = new UpdateUserDTO(7L, "jacintoXT", duplicatedEmail, "jacinto",
+                "rua dos pés de jaca, 146", Role.OWNER);
+
+        when(repository.count(fieldName, duplicatedEmail)).thenReturn(1L);
+
+        // when
+        Executable updateWithDuplicatedEmail = () -> service.update(dto);
+
+        // then
+        assertThrows(DuplicatedEmailException.class, updateWithDuplicatedEmail);
+        verify(repository).count(fieldName, duplicatedEmail);
+    }
+
+    @Test
+    void shouldThrowDuplicatedLoginExceptionWhenDuplicatedLogin() {
+        // given
+        String fieldName = "login";
+        String duplicatedLogin = "pedro321";
+
+        UpdateUserDTO dto = new UpdateUserDTO(7L, duplicatedLogin, "jacinto@test.com.br", "jacinto",
+                "rua dos pés de jaca, 146", Role.OWNER);
+
+        when(repository.count(fieldName, duplicatedLogin)).thenReturn(1L);
+
+        // when
+        Executable updateWithDuplicatedLogin = () -> service.update(dto);
+
+        // then
+        assertThrows(DuplicatedLoginException.class, updateWithDuplicatedLogin);
+        verify(repository).count(fieldName, duplicatedLogin);
+    }
+
+    @Test
+    void shouldDeleteWhenDelete() {
         // given
         Long id = 1L;
-        
+
         // when
         service.delete(id);
 
         // then
         verify(repository).delete(id);
-    }
-
-    @Test
-    void shouldCountWhenCount(){
-        // given
-        String email = "something@test.com.br";
-        
-        
-        // when
-        service.count(email);
-
-        // then
-        verify(repository).count(email);
     }
 }
