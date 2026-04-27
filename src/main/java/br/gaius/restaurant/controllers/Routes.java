690395daee6369959a79498848ffe4afd3b5a246
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-04-26 22:16:15 -0300

    feat: acrescentando rotas com parâmetros opcionais.

--- src/main/java/br/gaius/restaurant/controllers/Routes.java
+++ src/main/java/br/gaius/restaurant/controllers/Routes.java
@@ -5,6 +5,8 @@ public class Routes {
     public static final String BASE_URL = "/api";
     public static final String VERSION = BASE_URL + "/v1";
     public static final String USER_RESOURCE = VERSION + "/users";
+    public static final String WITH_PAGING = USER_RESOURCE + "?page={page}?size={size}";
+    public static final String WITH_NAME = USER_RESOURCE + "?name={name}" + WITH_PAGING;
     public static final String WITH_ID = USER_RESOURCE + "/{id}";
     public static final String PWD_RESOURCE = WITH_ID + "/password";
 
