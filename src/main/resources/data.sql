8e7cccbe0be32d9c9ee120d49456969ccf469fd0
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-04-27 00:20:34 -0300

    chore: atualizando massa de dados inserida ao iniciar a aplicação para corresponder ao ambiente de teste.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,8 @@
-INSERT INTO `USER` (`login`, `password`, email, `name`, `address`, last_modified, user_type) 
+INSERT INTO `USER` (`login`, `password`, email, `name`, `address`, last_modified, `role`) 
 VALUES 
-    ('pedro321', 'X321kHEz', 'pedro@gmail.com', 'pedro', 'rua das acácias, 12', '2026-04-01', 'customer'),
-    ('lucasXyz', 'Wa3wuQy4', 'lucas@gmail.com', 'lucas', 'rua das oliveiras, 22', '2026-04-02', 'customer'),
-    ('maria1010', 'M4YP14uF', 'maria@gmail.com', 'maria', 'rua das cerejeiras, 32', '2026-04-03', 'customer'),
-    ('joaoBigBoss', 'udWP50kX', 'joao.bosco@gmail.com', 'joao', 'rua dos ipes, 42', '2026-04-04', 'owner'),
-    ('marianaDona', '3A49yr9C', 'mariana.nascimento@gmail.com', 'mariana', 'rua das magnólias, 52', '2026-04-05', 'owner');
+    ('pedro321', 'X321kHEz', 'pedro@gmail.com', 'pedro', 'rua das acácias, 12', '2026-04-01', 'CUSTOMER'),
+    ('lucasXyz', 'Wa3wuQy4', 'lucas@gmail.com', 'lucas', 'rua das oliveiras, 22', '2026-04-02', 'CUSTOMER'),
+    ('maria1010', 'M4YP14uF', 'maria@gmail.com', 'maria', 'rua das cerejeiras, 32', '2026-04-03', 'CUSTOMER'),
+    ('joaoBigBoss', 'udWP50kX', 'joao.bosco@gmail.com', 'joao', 'rua dos ipes, 42', '2026-04-04', 'OWNER'),
+    ('marianaDona', '3A49yr9C', 'mariana.nascimento@gmail.com', 'mariana', 'rua das magnólias, 52', '2026-04-05', 'OWNER'),
+    ('maria2020', 'bxBc99VY', 'maria.joana@gmail.com', 'maria', 'rua dos mandacarus, 62', '2026-04-03', 'OWNER');
