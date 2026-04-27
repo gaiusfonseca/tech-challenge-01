720db8c827f45fdb04f47c93d05c1656adbc9ed9
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-04-27 00:07:31 -0300

    fix: atualizando constantes para fazer o mapeamento correto.

--- src/main/java/br/gaius/restaurant/controllers/Routes.java
+++ src/main/java/br/gaius/restaurant/controllers/Routes.java
@@ -2,12 +2,11 @@ package br.gaius.restaurant.controllers;
 
 public class Routes {
 
-    public static final String BASE_URL = "/api";
-    public static final String VERSION = BASE_URL + "/v1";
-    public static final String USER_RESOURCE = VERSION + "/users";
-    public static final String WITH_PAGING = USER_RESOURCE + "?page={page}?size={size}";
-    public static final String WITH_NAME = USER_RESOURCE + "?name={name}" + WITH_PAGING;
-    public static final String WITH_ID = USER_RESOURCE + "/{id}";
+    public static final String BASE_URL = "/api/v1";
+    public static final String USER_RESOURCE = BASE_URL + "/users";
+    public static final String WITH_PAGING = "?page={page}?size={size}";
+    public static final String WITH_NAME = "?name={name}" + WITH_PAGING;
+    public static final String WITH_ID = "/{id}";
     public static final String PWD_RESOURCE = WITH_ID + "/password";
 
 
