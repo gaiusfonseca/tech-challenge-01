1c4121b9436c7961b8b370f413e430870657a48a
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-04-26 22:49:31 -0300

    refactor: removendo o Long id do UpdateUserDTO.

--- src/main/java/br/gaius/restaurant/dtos/UpdateUserDTO.java
+++ src/main/java/br/gaius/restaurant/dtos/UpdateUserDTO.java
@@ -2,6 +2,6 @@ package br.gaius.restaurant.dtos;
 
 import br.gaius.restaurant.entities.Role;
 
-public record UpdateUserDTO(Long id, String login, String email, String name, String address, Role role) {
+public record UpdateUserDTO(String login, String email, String name, String address, Role role) {
 
 }
