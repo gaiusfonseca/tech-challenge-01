46f0eb7b1df206a1602049b84d3dab58f7e0dc10
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-05-02 21:27:27 -0300

    chore: removendo imports não utilizados.

--- src/test/java/br/gaius/restaurant/services/UserServiceTest.java
+++ src/test/java/br/gaius/restaurant/services/UserServiceTest.java
@@ -3,8 +3,6 @@ package br.gaius.restaurant.services;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
