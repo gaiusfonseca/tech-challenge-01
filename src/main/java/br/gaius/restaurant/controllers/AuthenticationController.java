d48cb2349afd519b63bc81df24928692b2bf75d6
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-05-03 22:46:27 -0300

    feat: Implementando AuthenticationController para simular uma autenticação.

---
+++ src/main/java/br/gaius/restaurant/controllers/AuthenticationController.java
@@ -0,0 +1,29 @@
+package br.gaius.restaurant.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.gaius.restaurant.dtos.AuthenticationRequestDTO;
+import br.gaius.restaurant.services.AuthenticationService;
+
+
+@RestController
+@RequestMapping(Routes.BASE_URL + Routes.AUTH)
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    public AuthenticationController(AuthenticationService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> login(@RequestBody AuthenticationRequestDTO auth) {
+        service.authenticate(auth);
+        return ResponseEntity.noContent().build();
+    }
+    
+}
