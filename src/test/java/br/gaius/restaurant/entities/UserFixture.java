f2f020716e84bb41620466d0a0bb96b47814f92e
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-05-03 22:48:50 -0300

    feat: incluindo getAuthRequestDTO.

--- src/test/java/br/gaius/restaurant/entities/UserFixture.java
+++ src/test/java/br/gaius/restaurant/entities/UserFixture.java
@@ -2,6 +2,7 @@ package br.gaius.restaurant.entities;
 
 import java.time.LocalDate;
 
+import br.gaius.restaurant.dtos.AuthenticationRequestDTO;
 import br.gaius.restaurant.dtos.CreateUserDTO;
 import br.gaius.restaurant.dtos.UpdateUserDTO;
 import br.gaius.restaurant.dtos.UserResponseDTO;
@@ -24,11 +25,15 @@ public class UserFixture {
                 "rua do rei dos piratas, 7", LocalDate.now(), Role.CUSTOMER);
     }
 
+    public static AuthenticationRequestDTO getAuthRequestDTO() {
+        return new AuthenticationRequestDTO("mugiwara.luffy", "supersecret");
+    }
+
     public static User getUser() {
         return User.builder()
             .withId(7L)
             .withLogin("mugiwara")
-            .withPassword("supersecret")
+            .withPassword("$2a$10$l8YeqmuQmkzz1a2RjVE8COZA4oPO6VymmAc7Oh6U/lzHMG0IeMRyG")
             .withEmail("mugiwara.luffy@test.com")
             .withName("luffy")
             .withAddress("rua do rei dos piratas, 7")
