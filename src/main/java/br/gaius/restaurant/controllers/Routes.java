68a9a5eadf16ee7fd12529f21274d3d291c12d91
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-05-03 22:57:48 -0300

    refactor: removendo rotas nao utilizadas e renomeando para ficar mais semantico.

--- src/main/java/br/gaius/restaurant/controllers/Routes.java
+++ src/main/java/br/gaius/restaurant/controllers/Routes.java
@@ -3,11 +3,10 @@ package br.gaius.restaurant.controllers;
 public class Routes {
 
     public static final String BASE_URL = "/api/v1";
-    public static final String USER_RESOURCE = BASE_URL + "/users";
-    public static final String WITH_PAGING = "?page={page}?size={size}";
-    public static final String WITH_NAME = "?name={name}" + WITH_PAGING;
+    public static final String USERS = BASE_URL + "/users";
     public static final String WITH_ID = "/{id}";
-    public static final String PWD_RESOURCE = WITH_ID + "/password";
+    public static final String PASSWORDS = WITH_ID + "/passwords";
+    public static final String AUTHENTICATIONS = "/authentications";
 
 
 }
