b70c75ce6c1099557c3f88041b322f18cfe40ecd
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-05-03 23:01:11 -0300

    feat: acrescentando novo template de mensagem e construtor para reportar um UserNotFoundException pelo id.

--- src/main/java/br/gaius/restaurant/exceptions/UserNotFoundException.java
+++ src/main/java/br/gaius/restaurant/exceptions/UserNotFoundException.java
@@ -2,10 +2,15 @@ package br.gaius.restaurant.exceptions;
 
 public class UserNotFoundException extends RuntimeException {
 
-    private static String template = "No user found with login: '%s'.";
+    private static String idTemplate = "No user found with id: '%d'.";
+    private static String LoginTemplate = "No user found with login: '%s'.";
+
+    public UserNotFoundException(Long id){
+        super(String.format(idTemplate, id));
+    }
 
     public UserNotFoundException(String login){
-        super(String.format(template, login));
+        super(String.format(LoginTemplate, login));
     }
 
 }
