f438431d178f2a652c50b7f51e44b497b69bc25b
Gaius Fonseca <gaiusfonseca@gmail.com>
2026-05-04 00:29:15 -0300

    chore: atualizando dados de amostra para utilizar senha criptografada.

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,8 +1,15 @@
 INSERT INTO `USER` (`login`, `password`, email, `name`, `address`, last_modified, `role`) 
 VALUES 
-    ('pedro321', 'X321kHEz', 'pedro@gmail.com', 'pedro', 'rua das acácias, 12', '2026-04-01', 'CUSTOMER'),
-    ('lucasXyz', 'Wa3wuQy4', 'lucas@gmail.com', 'lucas', 'rua das oliveiras, 22', '2026-04-02', 'CUSTOMER'),
-    ('maria1010', 'M4YP14uF', 'maria@gmail.com', 'maria', 'rua das cerejeiras, 32', '2026-04-03', 'CUSTOMER'),
-    ('joaoBigBoss', 'udWP50kX', 'joao.bosco@gmail.com', 'joao', 'rua dos ipes, 42', '2026-04-04', 'OWNER'),
-    ('marianaDona', '3A49yr9C', 'mariana.nascimento@gmail.com', 'mariana', 'rua das magnólias, 52', '2026-04-05', 'OWNER'),
-    ('maria2020', 'bxBc99VY', 'maria.joana@gmail.com', 'maria', 'rua dos mandacarus, 62', '2026-04-03', 'OWNER');
+    ('pedro321', '$2a$10$KbsJA7Uvs2Yf5K4G6IwXH.2jakt6NYP/Pr9QAtZuUgtwgYo8jmFtS', 'pedro@gmail.com', 'pedro', 'rua das acácias, 12', '2026-04-01', 'CUSTOMER'),
+    ('lucasXyz', '$2a$10$la23i3e3vsObrPqOnPXdr.XqNjRC1/veWReFtHd/AxGRX23SGgfbW', 'lucas@gmail.com', 'lucas', 'rua das oliveiras, 22', '2026-04-02', 'CUSTOMER'),
+    ('maria1010', '$2a$10$U6WqxQsBWOHP0Pe4Z7HzT.excaocwLxitxVCEU0iE9vBOjjdDOzH2', 'maria@gmail.com', 'maria', 'rua das cerejeiras, 32', '2026-04-03', 'CUSTOMER'),
+    ('joaoBigBoss', '$2a$10$mHkQ/plBs.ZGIrbiPcoSROgah.FPzgA2quHkB7ClPpZhIgHNC.752', 'joao.bosco@gmail.com', 'joao', 'rua dos ipes, 42', '2026-04-04', 'OWNER'),
+    ('marianaDona', '$2a$10$rSQOHJ.OKRA7T.SAll.THOBgdZQz1J2SKh4CJXp8AeiOKGI/s.jbu', 'mariana.nascimento@gmail.com', 'mariana', 'rua das magnólias, 52', '2026-04-05', 'OWNER'),
+    ('maria2020', '$2a$10$LQl2BcgY3CRES54u1laMzeSIupRoBZrrerCZI6A5dkYLHUxBsOkkK', 'maria.joana@gmail.com', 'maria', 'rua dos mandacarus, 62', '2026-04-03', 'OWNER');
+
+    -- X321kHEz
+    -- Wa3wuQy4
+    -- M4YP14uF
+    -- udWP50kX
+    -- 3A49yr9C
+    -- bxBc99VY
